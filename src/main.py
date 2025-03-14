a145259262869563acbf288f86374eb93d437e42
Benjamin Leon <benjamin.leon@gravicore.io>
2025-03-13 19:58:54 -0700

    Updated readability
    Added linebreaks, removed extra imports, removed testing comments, added TODO

--- src/main.py
+++ src/main.py
@@ -1,19 +1,25 @@
-from pyautogui import move, moveTo, keyDown, keyUp
+from pyautogui import move, keyDown, keyUp
 from time import sleep
 from random import choice, random
+
 print("Welcome to my pyautogui anti-AFK script")
+
 print("what do you want the max delay between button presses to be?")
 max_delay_time = input('Maxmimum Delay Time: ')
+
 print("what do you want the maxmimum movement of the cursor to be?")
 max_movement = input('Maxmimum Cursor movement: ')
+
 print("what do you want the cursor time multiplier to be?")
 cursor_time_mult = input('Cursor time multiplier: ')
+
 print("how long do you want movement keys to be held down?")
 key_hold_time = input('Key Hold Time: ')
-# choices = ["up", "down", "left", "right"]
+
 choices = ["up", "down", "left", "right", "forward", "backwards", "mleft", "mright"]
 
 print("Pyautogui is running anti-AFK, press CTRL-C to exit")
+
 while 1:
     sleep(random() * float(max_delay_time))
     val = choice(choices)
